04bcdb584c925f0dc1b15a130ab5f002361e3207
Alice Moraes <alicefms@hotmail.com>
2021-06-26 17:44:14 -0300

    persiste questions relacionando à sala

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,4 +1,7 @@
+const Database = require('../db/config')
+
 module.exports = {
+
     index(req, res) {
         const roomId = req.params.room
         const questionId = req.params.question
@@ -7,6 +10,24 @@ module.exports = {
 
         console.log(`room = ${roomId}, question: ${questionId}
         action = ${action} senha: ${password}`)
+    },
+
+    async create(req, res) {
+        const db = await Database()
+        const question = req.body.question
+        const roomId = req.params.room
+
+        await db.run(`INSERT INTO questions (
+            title,
+            room,
+            read
+        ) VALUES (
+            "${question}",
+            ${roomId},
+            0
+        )`)
+
+        res.redirect(`/room/${roomId}`)
     }
 
 
--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,22 +5,38 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId = ''
+        let isRoom = true
+        while (isRoom) {
 
-        for (var i = 0; i < 6; i++) {
-            roomId += Math.floor(Math.random() * 10).toString()
+            for (var i = 0; i < 6; i++) {
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+
+            if (!isRoom) {
+                await db.run(`INSERT INTO rooms (
+                id,
+                pass
+            )VALUES(
+                ${parseInt(roomId)},
+                ${pass}
+            )`)
+            }
+
         }
 
-        db.run(`INSERT INTO rooms (
-            id,
-            pass
-        )VALUES(
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
+
 
         await db.close()
 
         res.redirect(`/room/${roomId}`)
+    },
+
+    open(req, res) {
+        const roomId = req.params.room
+        res.render('room', { roomId: roomId })
     }
 
 
